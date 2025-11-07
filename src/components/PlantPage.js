ce30c483f0e294a633512b94f90801dbacbeb119
mwabali <martinmwash18952058@gmail.com>
2025-11-07 20:55:49 +0300

    "added the sold out ID and logic"

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -5,8 +5,8 @@ import Search from "./Search";
 const API_URL = "http://localhost:6001/plants";
 
 function PlantPage() {
-  const [plants, setplants] = useState([]);
-  const [search, setSearch] = useState("");
+  const [plants, setplants] = React.useState([]);
+  const [search, setSearch] = React.useState("");
 
   useEffect(() => {
     fetch(API_URL)
@@ -18,11 +18,39 @@ function PlantPage() {
     .catch((err) => console.error("Error fetching plants:", err));
   }, []);
 
+  function handleAddPlant(plantObj) {
+        
+        fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(plantObj),
+        })
+            .then((r) => r.json())
+            .then((newPlant) => {
+                
+                setplants((prev) => [...prev, { ...newPlant, soldOut: false }]);
+            })
+            .catch((err) => console.error("Failed to add plant:", err));
+    }
+
+      function toggleSoldOut(id) {
+
+        setplants((prev) =>
+            prev.map((p) => (p.id === id ? { ...p, soldOut: !p.soldOut } : p))
+        );
+    }
+
+     const filteredPlants = plants.filter((p) =>
+        p.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList /> 
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} toggleSoldOut={toggleSoldOut} />
     </main>
   );
 }
