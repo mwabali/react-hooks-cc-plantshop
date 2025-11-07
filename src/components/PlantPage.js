1f3666c54e5245f5909e552bc08085a568bf0218
mwabali <martinmwash18952058@gmail.com>
2025-11-07 20:39:33 +0300

    "Added the fetch "

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const API_URL = "http://localhost:6001/plants";
 
 function PlantPage() {
+  const [plants, setplants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const normalized = data.map(p => ({...p, soldOut: p.soldOut ?? false}));
+      setplants(normalized);
+    })
+    .catch((err) => console.error("Error fetching plants:", err));
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
