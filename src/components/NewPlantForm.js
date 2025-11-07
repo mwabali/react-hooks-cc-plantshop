dfe9a867de712a4c9a099eacd3cc3a9f97b1d213
mwabali <martinmwash18952058@gmail.com>
2025-11-07 21:01:56 +0300

    "Added the form logic"

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,15 +1,54 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm(onAddPlant) {
+   const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [price, setPrice] = useState("");
+  function handleSubmit(e) {
+        e.preventDefault();
+        
+        if (!name.trim() || !image.trim() || !price) return;
+        const newPlant = {
+            name: name.trim(),
+            image: image.trim(),
+            price: parseFloat(price),
+        };
+        onAddPlant(newPlant)
+        setName("");
+        setImage("");
+        setPrice("");
+    }
+
+
+
   return (
     <div className="new-plant-form">
-      <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
-        <button type="submit">Add Plant</button>
-      </form>
+        <h2>Add a plant</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Plant name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    name="image"
+                    placeholder="Image URL"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                />
+                 <input
+                    type="number"
+                    name="price"
+                    step="0.01"
+                    placeholder="Price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+                <button type="submit">Add Plant</button>
+            </form>
     </div>
   );
 }
